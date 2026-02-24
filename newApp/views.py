b111ba7cdb7c4ac896a505eb4b3e9a0eeb00cbd3
Christina <christina@Christinas-MacBook-Air.local>
2026-02-24 13:42:42 -0600

    User registration and login

--- newApp/views.py
+++ newApp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
     return render(request, "newApp/home.html")
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect("login")  # or "home"
+    else:
+        form = UserCreationForm()
+
+    return render(request, "newApp/register.html", {"form": form})
+
