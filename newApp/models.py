fc1fe0cc514c1ec698658117e5777320ef45ee87
lunaad <126724656+lunaad@users.noreply.github.com>
2026-03-26 19:25:04 +0000

    dashboard image

--- newApp/models.py
+++ newApp/models.py
@@ -9,4 +9,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Course(models.Model):from django.db import models
+
+class MyModel(models.Model):
+    image = models.ImageField(upload_to='images/')
     
